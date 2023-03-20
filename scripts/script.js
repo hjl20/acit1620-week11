73c3782d6dbbba6cca6fd7013cc70e2190d9172b
Herman L <hluo21@my.bcit.ca>
2023-03-20 16:10:42 -0700

    Add li to ul with js - slide 15

--- scripts/script.js
+++ scripts/script.js
@@ -13,25 +13,38 @@
 
 // greet('john')
 
-const radiusPara = document.querySelector("#radius")
-const resultPara = document.querySelector("#result")
-console.log(radiusPara)
-console.log(resultPara)
+// const radiusPara = document.querySelector("#radius")
+// const resultPara = document.querySelector("#result")
+// console.log(radiusPara)
+// console.log(resultPara)
 
-const calcCircleArea = radius => {
-    radiusPara.textContent = radiusPara.textContent.concat(` is: ${radius}`)   
-    if (isNaN(radius)) {
-        resultPara.textContent = `Error: ${radius} is not a number`
-    }
-    else {
-        area = (Math.PI * radius**2).toFixed(2)
-        resultPara.textContent = `The area of a circle with radius ${radius} is: ${area}`
-        return area
+// const calcCircleArea = radius => {
+//     radiusPara.textContent = radiusPara.textContent.concat(` is: ${radius}`)   
+//     if (isNaN(radius)) {
+//         resultPara.textContent = `Error: ${radius} is not a number`
+//     }
+//     else {
+//         area = (Math.PI * radius**2).toFixed(2)
+//         resultPara.textContent = `The area of a circle with radius ${radius} is: ${area}`
+//         return area
+//     }
+// }
+
+// let radius = prompt("Enter a radius:")
+// calcCircleArea(radius)
+
+
+
+// const ul = document.getElementsByClassName("shopping")[0]
+const ul = document.querySelector(".shopping")
+const populateShoppingList = arrayItems => {
+    for (item of arrayItems) {
+        let li = document.createElement("li")
+        // li.textContent = item
+        li.appendChild(document.createTextNode(item))
+        ul.appendChild(li)
     }
 }
 
-let radius = prompt("Enter a radius:")
-calcCircleArea(radius)
-
-
-
+const myList = ["cheese", "bread", "green pepper"]
+populateShoppingList(myList)
