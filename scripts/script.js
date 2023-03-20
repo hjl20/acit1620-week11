5d57c5d202e58ec26153079b8732bea80a2169ec
Herman L <hluo21@my.bcit.ca>
2023-03-20 14:22:57 -0700

    Arrow function practice

--- scripts/script.js
+++ scripts/script.js
@@ -3,9 +3,19 @@
 //     console.log(`Hello ${name}`)
 // }
 
-const greet = (name, time="day") => {
-    console.log(`Good ${time} ${name}`)
+// const greet = function(name) {
+//     console.log(`Hello ${name}`)
+// }
+
+// const greet = (name, time="day") => {
+//     console.log(`Good ${time} ${name}`)
+// }
+
+// greet('john')
+
+
+const calcCircleArea = radius => {
+    alert(`The area of a circle with radius ${radius} is ${(Math.PI * radius**2).toFixed(2)}`)
 }
 
-greet('john')
-
+calcCircleArea(prompt("Enter a radius: "))
